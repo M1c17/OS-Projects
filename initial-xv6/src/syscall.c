96ae4348298719a7fd0ca7797d10a06621083b8f
M1c17 <pictor117@gmail.com>
2020-06-18 23:58:38 -0400

    some changes

--- initial-xv6/src/syscall.c
+++ initial-xv6/src/syscall.c
@@ -129,9 +129,7 @@ static int (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
-//----------------------------------
 [SYS_getreadcount]  sys_getreadcount,
-//----------------------------------
 };
 
 void
@@ -142,6 +140,8 @@ syscall(void)
 
   num = curproc->tf->eax;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
+    if (num == SYS_read)
+      myproc()->readcount++;
     curproc->tf->eax = syscalls[num]();
   } else {
     cprintf("%d %s: unknown sys call %d\n",
